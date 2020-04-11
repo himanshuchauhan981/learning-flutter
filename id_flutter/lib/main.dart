ea95045b10fcb67bdb783f12aa6444325423d82d
Himanshu Chauhan <himanshuchauhan981@gmail.com>
2020-04-11 13:39:08 +0530

    changed statless widget to stateful widget

--- id_flutter/lib/main.dart
+++ id_flutter/lib/main.dart
@@ -4,7 +4,14 @@ void main() =>runApp(MaterialApp(
   home: Card()
 ));
 
-class Card extends StatelessWidget {
+class Card extends StatefulWidget {
+  @override
+  _CardState createState() => _CardState();
+}
+
+class _CardState extends State<Card> {
+  int level = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,6 +21,17 @@ class Card extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.grey[850],
         elevation: 0.0,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            level +=1;
+          });
+        },
+        child:  Icon(
+          Icons.add
+        ),
+        backgroundColor: Colors.grey[800],
       ),
       body: Padding(
         padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
@@ -57,7 +75,7 @@ class Card extends StatelessWidget {
             ),
             SizedBox(height: 10.0),
             Text(
-              '8',
+              '$level',
               style: TextStyle(
                   color: Colors.amberAccent[200],
                   letterSpacing: 2.0,
@@ -89,4 +107,3 @@ class Card extends StatelessWidget {
     );
   }
 }
-
